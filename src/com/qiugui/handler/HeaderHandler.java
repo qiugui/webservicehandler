3d72a2fe84801d40a3626a9c70c0ee51b16db54f
qiugui <qiugui0557@live.com>
2015-03-30 01:06:38 +0800

    2015/03/30 01:00

--- src/com/qiugui/handler/HeaderHandler.java
+++ src/com/qiugui/handler/HeaderHandler.java
@@ -1,11 +1,20 @@
  package com.qiugui.handler;
 
+import java.io.IOException;
 import java.util.Set;
 
 import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPMessage;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+import org.w3c.dom.Node;
+
  public class HeaderHandler implements SOAPHandler<SOAPMessageContext> {
 
 	@Override
@@ -22,7 +31,34 @@ import javax.xml.ws.handler.soap.SOAPMessageContext;
 
 	@Override
 	public boolean handleMessage(SOAPMessageContext context) {
-		System.out.println("message"); 
+		try{
+			Boolean out = (Boolean) context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+			if(out){
+				SOAPMessage message = context.getMessage();
+				//判断message中是否有header
+				SOAPEnvelope envelope = message.getSOAPPart().getEnvelope();
+				
+				SOAPBody body = envelope.getBody();
+				Node node = body.getChildNodes().item(0);
+				String paramName = node.getLocalName();
+				//根据body内message的内容判断添加header与否
+				if("list".equals(paramName) || "addUser".equals(paramName)){
+					SOAPHeader header = envelope.getHeader();
+					if(header == null) header = envelope.addHeader();
+					
+					QName qName = new QName("http://service.qiugui.com/", "licenseInfo");
+					header.addHeaderElement(qName).setValue("123123");
+					message.writeTo(System.out);
+				}
+				
+			}
+		} catch (SOAPException e){
+			e.printStackTrace();
+		} catch (IOException e) {
+			 e.printStackTrace();
+			 
+		}
+		
 		return true;
 		 
 	}
--- src/com/qiugui/service/Test.java
+++ src/com/qiugui/service/Test.java
@@ -1,14 +1,21 @@
  package com.qiugui.service;
+
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Holder;
  public class Test {
 
-	public static void main(String[] args) {
-		MyserviceImplService msi = new MyserviceImplService();
+	public static void main(String[] args) throws Exception{
+		URL url = new URL("http://127.0.0.1:9999/ns?wsdl");
+		QName qName = new QName("http://service.qiugui.com/", "MyserviceImplService");
+		MyserviceImplService msi = new MyserviceImplService(url,qName);
 		IMyservice ms = msi.getMyserviceImplPort();
-		try {
-			ms.login("admin", "111111");
-		} catch (UserException_Exception e) {
-			 e.printStackTrace();			 
-		}
+		User user = new User();
+		user.setId(11);user.setNickname("qiugui");
+		user.setPassword("222222");user.setUsername("QiuGui");
+		Holder<User> holderUser = new Holder<User>(user);
+		ms.addUser(holderUser);
 	}
 
 }
